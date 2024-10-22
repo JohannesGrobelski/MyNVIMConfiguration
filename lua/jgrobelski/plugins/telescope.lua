307a089e9f41b5c7fc4e4561f7355929c28d8a29
Johannes Grobelski <grobelskijohannes@gmail.com>
2024-10-22 13:41:37 +0200

    open telescope results in new buffer!

--- lua/jgrobelski/plugins/telescope.lua
+++ lua/jgrobelski/plugins/telescope.lua
@@ -25,15 +25,18 @@ return {
     })
 
     telescope.setup({
-      defaults = {
-        path_display = { "smart" },
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
-            ["<C-t>"] = trouble_telescope.open,
-          },
+        defaults = {
+            path_display = { "smart" },
+            mappings = {
+                i = {
+                    ["<CR>"] = function(bufnr)
+                        require("telescope.actions.set").select(bufnr, "tab")
+                    end,
+                    ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+                    ["<C-j>"] = actions.move_selection_next, -- move to next result
+                    ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
+                    ["<C-t>"] = trouble_telescope.open,
+                },
         },
       },
     })
